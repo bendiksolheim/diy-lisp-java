0d3a42d80c23191cb5c817fa62a20a0710dd6c00
Bendik Solheim <bendik@solheim.im>
2015-06-22 21:46:58 +0200

    Added domain specific exception: ParseException

---
+++ src/com/diylisp/exception/ParseException.java
@@ -0,0 +1,12 @@
+package com.diylisp.exception;
+
+/**
+ * Created by bendik solheim on 22/06/15.
+ */
+public class ParseException extends RuntimeException {
+
+    public ParseException(String message) {
+        super(message);
+    }
+
+}
